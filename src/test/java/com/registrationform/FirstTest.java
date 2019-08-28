65b4b2759c1bfd6ca6364bc0948bed64204bbf0c
jacek.chowaniec <jacek.chowaniec85@gmail.com>
2019-08-28 18:07:42 +0200

    selenide test

--- src/test/java/com/registrationform/FirstTest.java
+++ src/test/java/com/registrationform/FirstTest.java
@@ -1,6 +1,10 @@
 package com.registrationform;
 
+import com.codeborne.selenide.Selenide;
+import org.fest.assertions.Assertions;
 import org.testng.annotations.Test;
+
+import static com.codeborne.selenide.Selenide.open;
 
 public class FirstTest {
 
@@ -8,5 +12,14 @@ public class FirstTest {
     public void shouldPrintHelloWorld() {
         // then
         System.out.println("Hello World!");
+    }
+
+    @Test
+    public void shouldOpenWpHomePage() {
+        // when
+        open("http://wp.pl");
+
+        // then
+        Assertions.assertThat(Selenide.title()).isEqualTo("Wirtualna Polska - Wszystko co ważne - www.wp.pl");
     }
 }
