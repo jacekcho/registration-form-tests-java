4f0a42ee50d691079fd7675379e35daafd0ba6b9
jacek.chowaniec <jacek.chowaniec85@gmail.com>
2019-08-28 17:55:17 +0200

    init commit

---
+++ src/test/java/com/registrationform/FirstTest.java
@@ -0,0 +1,12 @@
+package com.registrationform;
+
+import org.testng.annotations.Test;
+
+public class FirstTest {
+
+    @Test
+    public void shouldPrintHelloWorld() {
+        // then
+        System.out.println("Hello World!");
+    }
+}
